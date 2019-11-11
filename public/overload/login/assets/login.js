67919142f4d032f317b1f50b5d932dc73413607a
Edinei J. Bauer <galera.org@gmail.com>
2019-11-11 11:56:33 -0300

    index login landing page

--- public/overload/login/assets/login.js
+++ public/overload/login/assets/login.js
@@ -42,12 +42,17 @@ function login() {
 }
 
 function saibamais() {
-    $('html, body').animate({
+    $("#parent").animate({
         scrollTop: $("#saibamais-content").offset().top
     }, 700);
 }
 
 function goToLogin() {
+
+    $("#parent").animate({
+        scrollTop: $("#background-login").offset().top
+    }, 1100);
+
     $("#login-acessar").remove();
     $("#login-sitename").removeClass("titleLogin");
     $("#div-saibamais").removeClass("saibamais");
@@ -70,24 +75,63 @@ $(function () {
 
     $("#login-card, #login-title").css("min-height", window.innerHeight + "px");
     /*$("#saibamais-content").css("margin-bottom", window.innerHeight + "px");
+*/
 
-    $(window).scroll(function () {
-        if($("#core-content").hasClass("r-login")) {
-            var scroll = $(window).scrollTop();
-            if (scroll > $(document).height() - (window.innerHeight * 1.5)) {
-                $("#login-container").css({"position": "fixed", "bottom": 0, "left": 0});
-                $("#saibamais-content").css("margin-top", window.innerHeight + "px");
+    $("#parent").scroll(function () {
+        let block1 = $("#block1").offset().top;
+        let block2 = $("#block2").offset().top;
+        let block3 = $("#block3").offset().top;
+        let block4 = $("#block4").offset().top;
+        let block5 = $("#block5").offset().top;
+        var scroll = $(this).scrollTop();
+        let animationCount = {
+            first: !1,
+            second: !1,
+            third: !1,
+            forth: !1,
+            fifth: !1,
+        };
 
-                let heightTop = $("#login-card")[0].clientHeight / 2;
-                $(".imgsaibamais").each(function (i, e) {
-                    heightTop += $(e)[0].clientHeight;
-                });
-                if (scroll > heightTop && $("#login-sitename").hasClass("titleLogin"))
-                    goToLogin();
-            } else {
-                $("#login-container").css({"position": "relative"});
-                $("#saibamais-content").css("margin-top", 0);
-            }
+        if (!animationCount.first && scroll > block1 + (window.innerHeight / 2.8)) {
+            animationCount.first = !0;
+            $(".title-block1:eq(1)").css("opacity", 1).addClass("animate-left animate-fading");
+            setTimeout(function () {
+                $(".title-block1:eq(0)").css("opacity", 1).addClass("animate-left animate-fading");
+            },200);
         }
-    });*/
+
+        if (!animationCount.second && scroll > block2 + (window.innerHeight / 2)) {
+            animationCount.second = !0;
+            $(".title-block2:eq(1)").css("opacity", 1).addClass("animate-right animate-fading");
+            setTimeout(function () {
+                $(".title-block2:eq(0)").css("opacity", 1).addClass("animate-right animate-fading");
+            },200);
+
+            $("#block2").find("img").addClass("active");
+        }
+
+        if (!animationCount.forth && scroll > block4 + window.innerHeight * 1.3 ) {
+            animationCount.forth = !0;
+            $(".title-block4:eq(1)").css("opacity", 1).addClass("animate-left animate-fading");
+            setTimeout(function () {
+                $(".title-block4:eq(0)").css("opacity", 1).addClass("animate-left animate-fading");
+            },200);
+        }
+
+        if (!animationCount.fifth && scroll > block5 + window.innerHeight * 1.8) {
+            animationCount.fifth = !0;
+            $(".title-block5:eq(1)").css("opacity", 1).addClass("animate-right animate-fading");
+            setTimeout(function () {
+                $(".title-block5:eq(0)").css("opacity", 1).addClass("animate-right animate-fading");
+            },200);
+        }
+
+        if (!animationCount.third && scroll > block3 + window.innerHeight * 2.5) {
+            animationCount.third = !0;
+            $(".title-block3:eq(1)").css("opacity", 1).addClass("animate-left animate-fading");
+            setTimeout(function () {
+                $(".title-block3:eq(0)").css("opacity", 1).addClass("animate-left animate-fading");
+            },200);
+        }
+    });
 });
